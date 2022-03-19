86094dfe3d31ec2082eac7ce1eaccfab0664aade
Jascha030 <contact@jaschavanaalst.nl>
2022-03-19 13:37:59 +0100

    💄 Full w/h

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,13 +2,14 @@
 	import "../app.css";
 </script>
 
-<main>
+<main class="w-full h-full">
 	<slot />
 </main>
 
 <style global>
 	html,
 	body {
+    @apply w-full h-full;
 		padding: 0;
 		margin: 0;
 	}
