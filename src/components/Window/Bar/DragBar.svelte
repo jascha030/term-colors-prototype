7511ff7ea1370fb046df0f057d9d40c2a9e97ab3
Jascha030 <contact@jaschavanaalst.nl>
2022-03-20 02:26:28 +0100

    💄 Add Dragbar with mac style traffic lights.

---
+++ src/components/Window/Bar/DragBar.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import TrafficLights from './TrafficLights.svelte';
+
+	export let textColor: string = '#ffffff';
+
+	$: textStyles = `color:${textColor}`;
+</script>
+
+<div class={'w-full h-8 content-center rounded-t-xl grid grid-cols-3'}>
+	<TrafficLights />
+	<div class="flex align-content-center justify-center">
+		<p class="align-self-center" style={textStyles}>Title</p>
+	</div>
+	<div />
+</div>
---
+++ src/components/Window/Bar/TrafficLights.svelte
@@ -0,0 +1,64 @@
+<script>
+</script>
+
+<div class="px-4">
+	<div class="flex">
+		<div class="traffic-lights-container align-self-begin flex flex-row gap-2">
+			<div>
+				<button class="traffic-light close" />
+			</div>
+			<div>
+				<button class="traffic-light minimize" />
+			</div>
+			<div>
+				<button class="traffic-light maximize" />
+			</div>
+		</div>
+	</div>
+</div>
+
+<style lang="scss">
+	$close-red: #ff6159;
+	$close-red-active: #bf4942;
+	$close-red-icon: #4d0000;
+	$close-red-icon-active: #190000;
+
+	$minimize-yellow: #ffbd2e;
+	$minimize-yellow-active: #bf8e22;
+	$minimize-yellow-icon: #995700;
+	$minimize-yellow-icon-active: #592800;
+
+	$maximize-green: #28c941;
+	$maximize-green-active: #1d9730;
+	$maximize-green-icon: #006500;
+	$maximize-green-icon-active: #003200;
+
+	$disabled-gray: #ddd;
+
+	.traffic-lights-container {
+		&:hover {
+			.traffic-light {
+				@apply transition-all ease-in-out duration-200;
+			}
+		}
+
+		.traffic-light {
+			@apply rounded-full;
+			height: 12px;
+			width: 12px;
+			/** background: $disabled-gray; **/
+		}
+
+		.close {
+			background: $close-red;
+		}
+
+		.minimize {
+			background: $minimize-yellow;
+		}
+
+		.maximize {
+			background: $maximize-green;
+		}
+	}
+</style>
