533c3a14b7539b923e79f6737ee5a9eb33695578
Jascha030 <contact@jaschavanaalst.nl>
2022-03-20 23:38:47 +0100

    🔥 Remove commented cr*p.

--- src/components/Window/Window.svelte
+++ src/components/Window/Window.svelte
@@ -6,7 +6,6 @@
 	<div class="rounded-xl drop-shadow-md aspect-video bg-ansi-black ratio-1/9 text-white w-auto m-auto max-h-full min-w-fit">
 		<DragBar />
 		<div id="content">
-			<!--<div class="absolute left-0 right-0 top-8 bottom-3/4 min-h-fit">-->
 			<div class="w-full min-w-fit">
 				<pre class="text-slate-200 text-center w-full min-h-fit">
 _____                       _____              ______
@@ -16,7 +15,6 @@ _  __/  _ \_  ___/_  __ `__ \_  /__  __ \  __ `/_  /
 \__/ \___//_/    /_/ /_/ /_//_/  /_/ /_/\__,_/ /_/   
         </pre>
 			</div>
-			<!--<div class="absolute left-0 right-0 bottom-8 top-1/4">-->
 			<div class="w-full flex-grow min-w-fit">
 				<div class="flex flex-col h-full w-full">
 					<div class="flex-grow p-2">other content</div>
