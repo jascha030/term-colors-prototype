a0357a81977251da95cd915160d129c12055595a
Jascha030 <contact@jaschavanaalst.nl>
2022-03-20 02:24:27 +0100

    💄 Create navbar

---
+++ src/components/Menu/Navbar.svelte
@@ -0,0 +1,31 @@
+<nav class="bg-gray-800">
+	<div class="w-full mx-auto px-2 sm:px-6 lg:px-8">
+		<div class="relative flex items-center justify-between h-16">
+			<div class="absolute inset-y-0 left-0 flex items-center">
+				<div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+					<div class="flex-shrink-0 flex items-center">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="h-6 w-6"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke="white"
+							stroke-width="2"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+							/>
+						</svg>
+					</div>
+					<div class="hidden sm:block sm:ml-6">
+						<div class="flex space-x-4">
+							<p class="text-white">Terminal colorscheme generator</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</nav>
