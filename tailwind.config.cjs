8d4526fbfe302ae305524a38d5d909ca16d0509c
Jascha030 <contact@jaschavanaalst.nl>
2022-03-21 00:27:58 +0100

    💄 Add ANSI colors to tailwind config.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -14,11 +14,26 @@ module.exports = {
         razzmatazz: '#ea1479',
         aliceBlue: '#4db6ac',
         smokey: '#eeeeee',
-
         ansi: {
           black: '#000000',
           red: '#cc0000',
-        }
+          green: '#4e9a06',
+          yellow: '#c4a000',
+          blue: '#729fcf',
+          magenta: '#75507b',
+          cyan: '#06989a',
+          white: '#d3d7cf',
+          bright: {
+            black: '#555753',
+            red: '#ef2929',
+            green: '#8ae234',
+            yellow: '#fce94f',
+            blue: '#32afff',
+            magenta: '#ad7fa8',
+            cyan: '#34e2e2',
+            white: '#ffffff',
+          }
+        },
       }
     }
   },
