40cf594ad03231385b0f5af038c2941e142c2587
Jascha030 <contact@jaschavanaalst.nl>
2022-03-20 02:24:36 +0100

    🚧 Start SideBar component

--- src/components/Menu/Sidebar.svelte
+++ src/components/Menu/Sidebar.svelte
@@ -3,9 +3,15 @@
 
 <aside id="sidebar">
   <div class="overflow-y-auto py-4 px-3">
-    <ul class="space-y-2">
-
+    <ul class="space-y-2 list-none">
+      
     </ul>
   </div>
 </aside>
+
+<style>
+  #sidebar {
+    @apply h-full bg-slate-500 flex-col;
+  }
+</style>
 
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,18 @@
 <script>
-  import Sidebar from "../components/Menu/Sidebar.svelte";
+	import Navbar from '../components/Menu/Navbar.svelte';
+	import Sidebar from '../components/Menu/Sidebar.svelte';
+	import Window from '../components/Window/Window.svelte';
 </script>
 
 <div class="w-full h-full bg-slate-100">
-  <Sidebar />
-</div>
+	<Navbar />
 
+	<div class="w-full absolute left-0 right-0 bottom-0 top-16 grid grid-cols-6">
+		<div class="col col-span-1">
+      <Sidebar />
+    </div>
+		<div class="bg-slate-300 col col-span-5">
+      <Window />
+    </div>
+	</div>
+</div>
