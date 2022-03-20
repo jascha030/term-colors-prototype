c6bb04b494bdbecf58ab28c0321ca39c62855bdf
Jascha030 <contact@jaschavanaalst.nl>
2022-03-20 23:35:24 +0100

    💄 Add colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,26 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        backgroundDim: '#34324A',
+        background: '#292D3E',
+        backgroundDarker: '#1a1423',
+        foregroundDim: '#676E95',
+        foreground: '#a6Accd',
+        accentViolet: '#a59bff',
+        sunshine: '#ffb833',
+        sunshineBright: '#ffcc00',
+        razzmatazz: '#ea1479',
+        aliceBlue: '#4db6ac',
+        smokey: '#eeeeee',
+
+        ansi: {
+          black: '#000000',
+          red: '#cc0000',
+        }
+      }
+    }
   },
   plugins: []
 };
