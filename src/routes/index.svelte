02ec41ad7510b1f50a687d3203f5b88b1710347b
Jascha030 <contact@jaschavanaalst.nl>
2022-03-20 23:35:42 +0100

    🚧 Set the stage

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,14 +4,14 @@
 	import Window from '../components/Window/Window.svelte';
 </script>
 
-<div class="w-full h-full bg-slate-100">
+<div class="w-full h-full bg-backgroundDarker">
 	<Navbar />
 
 	<div class="w-full absolute left-0 right-0 bottom-0 top-16 grid grid-cols-6">
 		<div class="col col-span-1">
       <Sidebar />
     </div>
-		<div class="bg-slate-300 col col-span-5">
+		<div class="col col-span-5 relative">
       <Window />
     </div>
 	</div>
